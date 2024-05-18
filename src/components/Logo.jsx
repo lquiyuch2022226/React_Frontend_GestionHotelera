d5f3f98f0915910aff11852863554d2ab5012e4b
lquiyuch2022226 <lquiyuch-2022226@kinal.edu.gt>
2024-05-18 12:59:00 -0600

    Se cambia el texto de la imagen

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/img/kha.jpeg'
 export const Logo = ({text}) => {
   return (
     <div className='auth-form-logo-container'>
-        <img src={logo} alt="Escudo" />
+        <img src={logo} alt="KHA" />
         <span>{text}</span>
     </div>
   )
