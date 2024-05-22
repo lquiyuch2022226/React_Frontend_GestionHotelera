a2bbea69dd73dd5dd4f5ce81dff263d65a2f79bf
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-05-22 06:59:11 -0600

    Se agrego un boton al form

--- src/pages/adminHotelPage/ComponentsAdHotel/Invoice/Invoice.jsx
+++ src/pages/adminHotelPage/ComponentsAdHotel/Invoice/Invoice.jsx
@@ -1,8 +1,13 @@
 import { HeaderAdHotel } from "../header/HeaderAdHotel";
+import { useUsersGet } from "../../../../shared/hooks";
 import './Invoice.css'
 
 export const Invoice = () => {
+    const {getUser} = useUsersGet()
 
+    const searchUser = () =>{
+      getUser()
+    }
     return (
       <div className="invoice-container">
         <div>
@@ -13,6 +18,7 @@ export const Invoice = () => {
             <h1>INVOICE</h1>
             <h2>EMAIL</h2>
             <input type="email" placeholder="text here"/>
+            <button className="button" onClick={searchUser}>search</button>
           </form>
         </div>
       </div>
