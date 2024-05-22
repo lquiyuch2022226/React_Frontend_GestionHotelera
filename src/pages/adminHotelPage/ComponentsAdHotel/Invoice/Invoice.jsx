63df9fc6234da893613774e72bda98ab7113c41a
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-05-22 06:49:29 -0600

    Se agrego el formulario para buscar a el usuario en factura

--- src/pages/adminHotelPage/ComponentsAdHotel/Invoice/Invoice.jsx
+++ src/pages/adminHotelPage/ComponentsAdHotel/Invoice/Invoice.jsx
@@ -1,18 +1,19 @@
 import { HeaderAdHotel } from "../header/HeaderAdHotel";
+import './Invoice.css'
 
 export const Invoice = () => {
 
     return (
       <div className="invoice-container">
         <div>
-        <div>
-            <HeaderAdHotel/>
-          </div>
+          <HeaderAdHotel/>
         </div>
         <div>
-            <form action="">
-                <h1>Hello nigga</h1>
-            </form>
+          <form className="form-container">
+            <h1>INVOICE</h1>
+            <h2>EMAIL</h2>
+            <input type="email" placeholder="text here"/>
+          </form>
         </div>
       </div>
     );
